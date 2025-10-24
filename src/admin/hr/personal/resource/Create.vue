251cee8ed82459f76e18b5e5e491ed1a311badd3
jorik2018 <antoniopinedo2018@gmail.com>
2025-10-23 22:41:21 -0500

    x

--- src/admin/hr/personal/resource/Create.vue
+++ src/admin/hr/personal/resource/Create.vue
@@ -33,14 +33,14 @@
       <v-calendar v-model="o.fechaAsignacion" />
 
       <div class="right" style="margin-top: 10px" v-if="o.fechaAsignacion && o.id">
-        <v-uploader icon="fa-file" ref="uploader" style="margin-top: 10px" value="Adjuntar documento"
+        <v-uploader icon="fa-file" ref="uploader" domain="alter" style="margin-top: 10px" value="Adjuntar documento"
           :click="uploaderClick" v-on:input="changeImage($event)"></v-uploader>
       </div>
       <label>Fecha Devolución:</label>
       <v-calendar v-model="o.fechaDevolucion" />
 
       <div class="right" style="margin-top: 10px" v-if="o.fechaDevolucion && o.id">
-        <v-uploader icon="fa-file" ref="uploader" style="margin-top: 10px" value="Adjuntar documento"
+        <v-uploader icon="fa-file" ref="uploader" domain="alter" style="margin-top: 10px" value="Adjuntar documento"
           :click="uploaderClick" v-on:input="changeImage($event)"></v-uploader>
       </div>
     </div>
