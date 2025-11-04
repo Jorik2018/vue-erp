033618d0b3fa860622dd3aceb64d7eca22ca0b23
jorik2018 <antoniopinedo2018@gmail.com>
2025-11-04 08:21:52 -0500

    x

--- src/admin/hr/movement/Create.vue
+++ src/admin/hr/movement/Create.vue
@@ -27,7 +27,7 @@
           <div style="margin-top: 10px; border: 1px solid gray;padding:5px" v-if="o.filename">
             {{o.filename}} {{o.tempFile}}
           </div>
-          <div class="right" style="margin-top: 10px">
+          <div class="right" style="margin: 10px 0px">
             <v-uploader icon="fa-file" ref="uploader" domain="alter" style="margin-top: 10px" value="Adjuntar documento"
               :click="uploaderClick" v-on:input="changeImage($event)"></v-uploader>
           </div>
