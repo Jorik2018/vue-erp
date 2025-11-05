edb9d5b1ad0d604d256f0b0927b116e5b3b0fa1a
jorik2018 <antoniopinedo2018@gmail.com>
2025-11-04 23:31:17 -0500

    x

--- src/admin/hr/movement/Create.vue
+++ src/admin/hr/movement/Create.vue
@@ -25,6 +25,7 @@
           <label>Documento:</label>
           <input v-model="o.actaAsignacion" />
           <div style="margin-top: 5px; border: 1px solid gray;padding:5px;border-radius: 8px;" v-if="o.filename">
+            <i class="fa-plus"></i>
             <a v-if="o.tempFile" :href="`/wp-content/uploads/temps/${o.tempFile}`" target="_">Ver Documento</a>
             <a v-else :href="`/wp-content/uploads/movements/${o.filename}`" target="_">Ver Documento</a>
           </div>
