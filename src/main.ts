029bed0f51e2b41fd4167ccf7d9eb1f6228aed20
jorik2018 <antoniopinedo2018@gmail.com>
2025-11-20 11:51:03 -0500

    x

--- src/main.ts
+++ src/main.ts
@@ -62,7 +62,7 @@ app
   .use(IsobitUI);
 
 router.isReady().then(() => {
-  initDB(34, [
+  initDB(35, [
     ["region", { src: "/api/directory/region/0/0" }],
     ["province", { keyPath: "code", src: "/api/directory/province/0/0" }],
     ["district", { keyPath: "code", src: "/api/directory/district/0/0" }],
