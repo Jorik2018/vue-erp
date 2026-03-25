450b2af842ba77a74c9610f390be6720781d462b
jorik2018 <antoniopinedo2018@gmail.com>
2026-03-25 10:43:30 -0500

    x

--- src/admin/payroll/View.vue
+++ src/admin/payroll/View.vue
@@ -98,7 +98,7 @@
                       v-model.number="item.values[cell.concept_id]" /-->
                     <!-- STRING -->
                     <!--input v-else type="text" v-model="item[cell.index]" class="v-input" /-->
-                    {{ item[cell.index] }}
+                    {{ item[cell.concept_id||cell.index] }}
                   </td>
                 </tr>
               </tbody>
